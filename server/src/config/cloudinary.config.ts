b3793ebeffd23a06d264b1db32f01448763140e3
Muhammad Talha <talharashid229@gmail.com>
2025-08-19 17:38:54 +0500

    Add Comment On Cloudinary Config

--- server/src/config/cloudinary.config.ts
+++ server/src/config/cloudinary.config.ts
@@ -6,10 +6,12 @@ if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
   throw new Error("Missing Cloudinary environment variables");
 }
 
+// Add cloudinary config
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
   api_key: CLOUDINARY_API_KEY,
   api_secret: CLOUDINARY_API_SECRET,
 });
+
 
 export default cloudinary;
